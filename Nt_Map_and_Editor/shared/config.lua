fcc3e34b64a38850227601040f4526a4e6dcd26c
Nubetastic <tgandolfo357@gmail.com>
2026-07-23 20:09:05 -0400

    Delete Nt_Map_and_Editor directory

--- Nt_Map_and_Editor/shared/config.lua
+++
@@ -1,5 +0,0 @@
-Config = Config or {}
-
--- Enable the in-game map editor for every player.
--- Set this to false on production servers; the configured map will still load.
-Config.EnableEditor = true
